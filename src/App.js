adf3efb473f6a2bdde1cb07b8fe7d2c3652315af
Pandhare Sambhaji S <69834917+sampee100@users.noreply.github.com>
2022-03-23 22:47:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1 className="container mt-4 mb-2">My Interview Portal</h1>
       <Routes>
-        <Route path={"/"} element={<Homepage/>} />
+        <Route path={"/root"} element={<Homepage/>} />
         <Route path={"/test/:id/:qid"} element={<Test/>} />
         <Route path={"/finish"} element={<Finish/>} />
       </Routes>
